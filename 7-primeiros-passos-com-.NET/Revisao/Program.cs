2bcd0e8c2718e3e180ec0560136ca8770a9f4abb
medeirosdanilo <medeirosdanilodev@gmail.com>
2021-12-10 19:37:51 -0300

    Commit de atualização da aula 07, criando os primeiros códigos em .Net

---
+++ 7-primeiros-passos-com-.NET/Revisao/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Insira o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine();
+                        Console.WriteLine("Informe a nota do aluno");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O Valor deve ser decimal!");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        break;
+
+                    case "2":
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
+                        break;
+
+                    case "3":
+                        //TODO: Calcular média geral
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bem vindo ao Programa, escolha uma das opções abaixo: ");
+            Console.WriteLine("1 - Inserir Novo Aluno");
+            Console.WriteLine("2 - Listar Alunos");
+            Console.WriteLine("3 - Calcular Média Geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string? opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
+
